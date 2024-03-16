0732c5b1a218f5ffdac76e94874e428ac8bf3d14
jorge975 <jcasillas245a@gmail.com>
2024-03-16 20:43:40 +0100

    Nueva modificacion

--- manage.sh
+++ manage.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Requires the database to be up
-FLASK_ENV=development DATABASE_URI=mysql://root:root@localhost/miproyecto python manage.py
+FLASK_ENV=development DATABASE_URI=mysql+mysqldb://root:root@localhost/miproyecto?unix_socket=/path/to/mysqld.sock python manage.py
