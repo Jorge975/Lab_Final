b5118d9e97fd7ccdf6c049dfaf4679eca013decc
jorge975 <jcasillas245a@gmail.com>
2024-03-16 20:05:18 +0100

    Nueva modificacion

--- app/config.py
+++ app/config.py
@@ -6,7 +6,7 @@ class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "your_secret_key")
 
     # Database configuration
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI", "mysql://root:root@localhost/miproyecto")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI", "mysql+mysqldb://root:root@localhost/miproyecto?unix_socket=/path/to/mysqld.sock")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # Add other configuration variables as needed
