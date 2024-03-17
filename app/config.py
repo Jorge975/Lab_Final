a5dd1757f91c88b79b93e48c26b8706548555ba9
jorge975 <jcasillas245a@gmail.com>
2024-03-17 02:05:41 +0100

    Nueva modificacion

--- app/config.py
+++ app/config.py
@@ -6,7 +6,7 @@ class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "your_secret_key")
 
     # Database configuration
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI", "mysql://root:root@localhost/miproyecto")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI", "postgresql://root:root@localhost/miproyecto")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # Add other configuration variables as needed
